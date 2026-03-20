d5f21c83598082fbc503ed5e03ec035c1ebe81e7
gabrielsimidamore <gabrielsipinheiro@gmail.com>
2026-03-20 14:30:35 -0300

    Update ThemeContext.tsx

--- src/contexts/ThemeContext.tsx
+++ src/contexts/ThemeContext.tsx
@@ -12,9 +12,9 @@ const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
 export const ThemeProvider = ({ children }: { children: ReactNode }) => {
   const [theme, setTheme] = useState<Theme>(() => {
     if (typeof window !== 'undefined') {
-      return (localStorage.getItem('theme') as Theme) || 'light';
+      return (localStorage.getItem('theme') as Theme) || 'dark';
     }
-    return 'light';
+    return 'dark';
   });
 
   useEffect(() => {
